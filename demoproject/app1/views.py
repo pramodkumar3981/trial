6eab8bc3caad1785cb084a9f8a5db4ce3204e2cb
rao3981 <Rao@3981>
2021-06-12 10:03:44 +0530

    no any

---
+++ demoproject/app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+from .models import*
+from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+
+# HOME PAGE
+def home(request):
+    return render(request,'base.html')
